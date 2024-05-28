9fb83ed92e6dd8082f803305cb22b8107604aff0
KeithLee <keithlee870507@gmail.com>
2024-05-28 23:14:42 +0800

    Add My First Semantic Kernel App

---
+++ SemanticKernel/MicrosoftLearn/SemanticKernelApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.SemanticKernel;
+
+// Create kernel
+var builder = Kernel.CreateBuilder();
+// Add a text or chat completion service using either:
+builder.Services.AddAzureOpenAIChatCompletion(
+    "Resuorce Name",
+    "Endpoint",
+    "Secret Key",
+    "Model Name");
+
+var kernel = builder.Build();
+
+var result = await kernel.InvokePromptAsync(
+        "Give me a list of breakfast foods with eggs and cheese");
+
+Console.WriteLine(result);
