d5e2ea52e6631e6efe1595f566cd1d1f16cb5f1f
KeithLee <keithlee870507@gmail.com>
2024-06-02 23:06:29 +0800

    Use TimePlugin

--- SemanticKernel/MicrosoftLearn/SemanticKernelApp/Program.cs
+++ SemanticKernel/MicrosoftLearn/SemanticKernelApp/Program.cs
@@ -1,17 +1,39 @@
-﻿using Microsoft.SemanticKernel;
+﻿
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.Plugins.Core;
 
-// Create kernel
-var builder = Kernel.CreateBuilder();
-// Add a text or chat completion service using either:
-builder.Services.AddAzureOpenAIChatCompletion(
-    "Resuorce Name",
-    "Endpoint",
-    "Secret Key",
-    "Model Name");
+# region Test to Connect AzureOpenAi API  
+// // Create kernel
+// var builder = Kernel.CreateBuilder();
+// // Add a text or chat completion service using either:
+// builder.Services.AddAzureOpenAIChatCompletion(
+//     "Resuorce Name",
+//     "Endpoint",
+//     "Secret Key",
+//     "Model Name");
 
-var kernel = builder.Build();
+// var kernel = builder.Build();
 
-var result = await kernel.InvokePromptAsync(
-        "Give me a list of breakfast foods with eggs and cheese");
+// var result = await kernel.InvokePromptAsync(
+//         "Give me a list of breakfast foods with eggs and cheese");
 
-Console.WriteLine(result);
+// Console.WriteLine(result);
+#endregion
+
+// #region TimePlugin
+// Sementic Kernel TimePlugin Sample
+var kernelBuilder = Kernel.CreateBuilder();
+
+kernelBuilder.Services.AddAzureOpenAIChatCompletion(
+    "Semantic-Kernel_Test",
+    "https://semantickernel.openai.azure.com/",
+    "2bc304e82df5497486d08f9b67cede41",
+    "gpt-35-turbo-16k");
+
+kernelBuilder.Plugins.AddFromType<TimePlugin>();
+var kernel = kernelBuilder.Build();
+var currentDay = await kernel.InvokeAsync("TimePlugin", "DayOfWeek");
+Console.WriteLine(currentDay);
+
+
+
