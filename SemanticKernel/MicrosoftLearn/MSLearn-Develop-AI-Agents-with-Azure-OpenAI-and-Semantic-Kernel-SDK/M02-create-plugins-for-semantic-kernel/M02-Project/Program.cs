fa59aad99f2a274712d7e206ddbd679dfd40eeec
KeithLee <keithlee870507@gmail.com>
2024-06-05 23:35:49 +0800

    Add ConversationSummaryPlgin Example

---
+++ SemanticKernel/MicrosoftLearn/MSLearn-Develop-AI-Agents-with-Azure-OpenAI-and-Semantic-Kernel-SDK/M02-create-plugins-for-semantic-kernel/M02-Project/Program.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.Plugins.Core;
+
+string yourDeploymentName = "Semantic-Kernel";
+string yourEndpoint = "https://semantickernel.openai.azure.com/";
+string yourKey = "";
+
+var builder = Kernel.CreateBuilder();
+builder.Services.AddAzureOpenAIChatCompletion(
+    yourDeploymentName,
+    yourEndpoint,
+    yourKey,
+    "gpt-4");
+
+// Add ConversationSummaryPlugin
+builder.Plugins.AddFromType<ConversationSummaryPlugin>();
+var kernel = builder.Build();
+
+// Setting the prompt
+string input = @"I'm a vegan in search of new recipes. 
+I love spicy food! Can you give me a list of breakfast 
+recipes that are vegan friendly?";
+
+var result = await kernel.InvokeAsync(
+    "ConversationSummaryPlugin", 
+    "GetConversationActionItems", 
+    new() {{ "input", input }});
+    
+Console.WriteLine(result);
